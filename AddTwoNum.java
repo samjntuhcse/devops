14ce294e497580a25c6e5d5fc0e3c40569c58888
samjntuhcse <samreshavena07@gmail.com>
2024-12-11 16:06:10 +0530

    Create AddTwoNum.java

---
+++ AddTwoNum.java
@@ -0,0 +1,13 @@
+public class AddTwoNum {
+  public static void main(String[] args) {
+    if(args.length <2) {
+      System.out.println("please provide two numbers as  arguments .");
+      return;
+    }
+     int num1 = Integer.parseInt(args[0]);
+     int num1 = Integer.parseInt(args[1]);
+    int sum = num1 + num2;
+    System.out.println("the sum of " + num1 + " and " + " is: " + sum);
+  }
+}         
+    
